03d66e3dd5e72335faf459e67ba4a3bff0ac4fa8
Kadambari Venkata Sai Anmol <11249a138@kanchiuniv.ac.in>
2025-11-06 01:26:59 -0800

    Create 1b.Binarysearch.c

---
+++ 1b.Binarysearch.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int binarySearch(int arr[], int n, int target) {
+int low = 0;
+int high = n - 1;
+while (low &lt;= high) {
+int mid = low + (high - low) / 2; // To avoid overflow
+if (arr[mid] == target) {
+return mid; // Found the target
+} else if (arr[mid] &lt; target) {
+low = mid + 1;
+} else {
+high = mid - 1;
+}
+}
+return -1; // Not found
+}
+int main() {
+int arr[] = {2, 3, 5, 6, 8};
+int n = sizeof(arr) / sizeof(arr[0]);
+int target = 6;
+int result = binarySearch(arr, n, target);
+if (result != -1) {
+printf(&quot;Element %d found at index %d\n&quot;, target, result);
+} else {
+printf(&quot;Element %d not found\n&quot;, target);
+}
+return 0;
+}
+Output
+Element 6 found at index 3
